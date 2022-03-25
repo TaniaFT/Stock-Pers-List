dce7dd0e0f298ef864b2a16fecbecca9c5908053
TaniaFT <tania.thomas@hotmail.co.uk>
2022-03-25 17:14:01 +0000

    Tweaks and adjustments based on failing products

--- perslist.py
+++ perslist.py
@@ -9,7 +9,7 @@ safechars = string.ascii_lowercase + string.ascii_uppercase + string.digits + '.
 per_page = 250
 
 r = requests.get(
-    'https://api.notonthehighstreet.com/api/v1/orders?token={}&state=dispatched&estimated_dispatch_at%5Bfrom%5D=2022-03-24T09%3A39%3A30%2B00%3A00&per_page='.format(
+    'https://api.notonthehighstreet.com/api/v1/orders?token={}&state=dispatched&estimated_dispatch_at%5Bfrom%5D=2022-03-25T09%3A24%3A35%2B00%3A00&per_page='.format(
         api_key)
     + str(per_page))
 callsNeeded = (r.json()["query"]["total"] / per_page) + 1
@@ -25,7 +25,7 @@ jewelList = []
 
 for x in range(0, int(callsNeeded)):
     r = requests.get(
-        'https://api.notonthehighstreet.com/api/v1/orders?token={}&state=dispatched&estimated_dispatch_at%5Bfrom%5D=2022-03-24T09%3A39%3A30%2B00%3A00&per_page='.format(
+        'https://api.notonthehighstreet.com/api/v1/orders?token={}&state=dispatched&estimated_dispatch_at%5Bfrom%5D=2022-03-25T09%3A24%3A35%2B00%3A00&per_page='.format(
             api_key)
         + str(per_page) + "&offset=" + str(per_page * x))
     for order in r.json()["data"]:
@@ -38,6 +38,7 @@ for x in range(0, int(callsNeeded)):
                     "photo" in item["item_title"].lower() or
                     "headband" in item["item_title"].lower() or
                     "bridesmaid" in item["item_title"].lower() or
+                    "key holder" in item["item_title"].lower() or
                     "keyring" in item["item_title"].lower()):
                 continue
 
@@ -85,20 +86,10 @@ for x in range(0, int(callsNeeded)):
                 newItem = perslistClasses.EmbroideredProductItem(item)
                 embList.append(newItem)
 
-            if "felt" in item["options"][1]["value"].lower():
-                newItem = perslistClasses.FeltedProductItem(item)
-                feltList.append(newItem)
-
-            if "bracelet" in item["item_title"].lower() or "necklace" in item["item_title"].lower() or "earrings" in item["item_title"].lower():
-                if "gift card" in item["options"][-2]["name"].lower() and "yes" in item["options"][-2]["value"].lower() and "birth" not in item["item_title"].lower():
-                    newItem = perslistClasses.JewelleryProductItem(item)
-                    jewelList.append(newItem)
-                if "globe" in item["item_title"].lower():
-                    newItem = perslistClasses.WorldJewelleryProductItem(item)
-                    jewelList.append(newItem)
-                if "gift card" in item["options"][-2]["name"].lower() and "yes" in item["options"][-2]["value"].lower() and "delicate birth" in item["item_title"].lower():
-                    newItem = perslistClasses.JewelleryBirthProductItem(item)
-                    jewelList.append(newItem)
+            if "scarf" in item["item_title"].lower() or "shawl" in item["item_title"].lower() or "blanket" in item["item_title"].lower():
+                if "felt" in item["options"][1]["value"].lower():
+                    newItem = perslistClasses.FeltedProductItem(item)
+                    feltList.append(newItem)
 
             if "personalis" in item["options"][0]["name"].lower() and "yes" in item["options"][0][
                 "value"].lower() and "embroider" not in item["options"][0]["value"].lower() and "felt" not in \
@@ -106,13 +97,26 @@ for x in range(0, int(callsNeeded)):
                 newItem = perslistClasses.BasicProductItem(item)
                 genList.append(newItem)
 
+            if "bracelet" in item["item_title"].lower() or "necklace" in item["item_title"].lower() or "earrings" in item[
+                "item_title"].lower():
+                if "gift card" in item["options"][-2]["name"].lower() and "yes" in item["options"][-2]["value"].lower() and "birth" not in item["item_title"].lower():
+                    newItem = perslistClasses.JewelleryProductItem(item)
+                    jewelList.append(newItem)
+                if "globe" in item["item_title"].lower():
+                    newItem = perslistClasses.WorldJewelleryProductItem(item)
+                    jewelList.append(newItem)
+                if "gift card" in item["options"][-2]["name"].lower() and "yes" in item["options"][-2][
+                    "value"].lower() and "delicate birth" in item["item_title"].lower():
+                    newItem = perslistClasses.JewelleryBirthProductItem(item)
+                    jewelList.append(newItem)
+
             if "personalis" in item["options"][1]["name"].lower() and "yes" in item["options"][1][
                 "value"].lower() and "embroider" not in item["options"][1]["value"].lower() and "felt" not in \
                     item["options"][1]["value"].lower():
                 newItem = perslistClasses.BasicProductItem(item)
                 genList.append(newItem)
 
-            if "socks" in item["item_title"].lower() and "name" in item["options"][0]["name"].lower():
+            if "socks" in item["item_title"].lower():
                 if "men" in item["item_title"].lower():
                     newItem = perslistClasses.MenSockProductItem(item)
                     sockList.append(newItem)
@@ -128,6 +132,8 @@ for x in range(0, int(callsNeeded)):
                 elif "name" in item["options"][0]["name"].lower():
                     newItem = perslistClasses.SockProductItem(item)
                     sockList.append(newItem)
+
+
 
 traceList = sorted(traceList, key=lambda TracingProductItem: TracingProductItem.personalisationString)
 traceA5List = sorted(traceA5List, key=lambda A5TracingProductItem: A5TracingProductItem.personalisationString)
